3a80444a9f59aa976935bba71e532e5719b18e87
rokon1014 <rokon.zaman1014@gmail.com>
2023-05-08 12:12:47 +1000

    working scripts

---
+++ terminal_dashboard.py
@@ -0,0 +1,279 @@
+"""
+TODO: Add description
+"""
+
+
+from dataclasses import dataclass, field
+from typing import List
+import curses
+
+
+@dataclass
+class PrintBlock:
+    string_with_attrib: List = field(default_factory=lambda: ["what to print", 0])
+    block_number: int = 1
+    starting_location_row: int = 0
+    starting_location_column: int = 0
+    title = str
+
+
+def assigning_attrib(var):
+    if var:
+        return TerminalDashboard.NOMINAL
+    else:
+        return TerminalDashboard.DANGER | curses.A_BOLD
+
+
+class TerminalDashboard:
+    def __init__(self):
+        # screens and windows
+        self.screen = curses.initscr()
+        self.num_rows, self.num_cols = self.screen.getmaxyx()
+        self.windows = None
+        self.current_line = 1
+        self.current_row = 1
+
+        # Colors
+        curses.start_color()
+        # curses.use_default_colors()
+        # if curses.has_colors():
+        #     for i in range(0, curses.COLORS):
+        #         curses.init_pair(i + 1, i, -1)
+
+        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_RED)
+        curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
+        curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+        curses.init_pair(4, curses.COLOR_MAGENTA, curses.COLOR_GREEN)
+        curses.init_pair(5, curses.COLOR_BLUE, curses.COLOR_BLACK)
+        curses.init_pair(6, curses.COLOR_CYAN, curses.COLOR_BLACK)
+        curses.init_pair(7, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+        curses.init_pair(8, curses.COLOR_BLACK, curses.COLOR_YELLOW)
+        curses.init_pair(9, curses.COLOR_BLACK, curses.COLOR_WHITE)
+
+        TerminalDashboard.DANGER = curses.color_pair(1) | curses.A_BOLD
+        TerminalDashboard.NOMINAL = curses.color_pair(2)
+        TerminalDashboard.WATCHOUT = curses.color_pair(3)
+        TerminalDashboard.MAGENTA = curses.color_pair(4)
+        TerminalDashboard.BLUE = curses.color_pair(5)
+        TerminalDashboard.CYAN = curses.color_pair(6)
+        TerminalDashboard.YELLOW = curses.color_pair(7)
+        TerminalDashboard.BAR_YELLOW = curses.color_pair(8)
+        TerminalDashboard.BAR_WHITE = curses.color_pair(9)
+
+        # cursor
+        curses.curs_set(0)
+
+    def cleanup(self):
+        curses.nocbreak()
+        self.screen.keypad(0)
+        curses.echo()
+        curses.endwin()
+        # raise
+
+    def all_refresh(self):
+        if self.windows:
+            for window in self.windows:
+                window.refresh()
+
+        else:
+            self.screen.refresh()
+
+    def all_erase(self):
+        if self.windows:
+            for window in self.windows:
+                window.erase()
+
+        else:
+            self.screen.erase()
+
+    def run(self):
+        try:
+            # print(curses.baudrate())
+            # print(curses.can_change_color())
+            # print(curses.color_content(2))
+
+            for i in range(0, 255):
+                self.screen.addstr(str(i), curses.color_pair(i))
+
+            # self.screen.addch("d", curses.A_BOLD)
+            self.screen.attron(curses.A_BOLD)
+            # self.screen.addch("e")
+
+            self.screen.bkgd(curses.A_ITALIC)
+            # self.screen.bkgdset(curses.A_BOLD)
+            # self.screen.addstr("esfdsd")
+            self.screen.border()
+            self.screen.box()
+
+            # while True:
+            #     self.num_rows, self.num_cols = self.screen.getmaxyx()
+
+            #     self.screen.addstr(
+            #         1, 1, f"rows: {self.num_rows}", curses.color_pair(10)
+            #     )
+            #     self.screen.addstr(
+            #         2, 1, f"Columns: {self.num_rows}", curses.color_pair(10)
+            #     )
+            #     self.screen.attron(curses.A_BOLD)
+            #     self.screen.border()
+            #     self.screen.box()
+
+            #     self.all_refresh()
+            #     self.all_erase()
+        except:
+            print(curses.error)
+            self.cleanup()
+            raise
+
+        self.screen.getch()
+
+    def print_info(self, blocks, row):
+        try:
+            # checking max num of line and columns
+            # checking for resize
+            self.num_rows, self.num_cols = self.screen.getmaxyx()
+
+            self.current_line = 1
+
+            if row > 1:
+                self.current_row = int(self.num_cols / row)
+            else:
+                self.current_row = 1
+
+            # iterate through each block
+            for block in blocks:
+                # Empty line at the start of each block
+                self.current_line += 1
+
+                # Iterate through each string in current block
+                for k, string_to_print in enumerate(block.string_with_attrib):
+                    # print(k)
+                    # check for resize
+                    if (
+                        self.current_line < self.num_rows
+                        and len(string_to_print[0]) < self.num_cols
+                    ):
+                        # First string is always the title
+                        if k == 1:
+                            attrib = string_to_print[1] | curses.A_BOLD
+                        else:
+                            attrib = string_to_print[1]
+
+                        # finally printing the string
+                        self.screen.addstr(
+                            self.current_line,
+                            self.current_row,
+                            f"{string_to_print[0]}",
+                            attrib,
+                        )
+
+                        # go to the next line
+                        self.current_line += 1
+
+            self.screen.border()
+            self.screen.box()
+
+            # self.all_refresh()
+            # self.all_erase()
+
+        except:
+            print(curses.error)
+            self.cleanup()
+            raise
+
+    def draw_bar(self, title="title", value=0.0, color=curses.A_BOLD, attrib="normal"):
+        try:
+            self.num_rows, self.num_cols = self.screen.getmaxyx()
+
+            self.current_line += 2
+            full_bar = int(self.num_cols * 0.7)
+            current_bar = int(full_bar * value)
+            ch = " "
+
+            if attrib is not ("normal" or "blink"):
+                raise AssertionError(" DRAW-BAR: Chose attrib between normal or blink")
+
+            if attrib == "normal":
+                attrib = curses.A_NORMAL | color
+            elif attrib == "blink":
+                attrib = curses.A_BLINK | color
+            else:
+                attrib = curses.A_NORMAL | color
+
+            if (
+                self.current_line < self.num_rows
+                and (current_bar + len(title) + 3) < self.num_cols
+            ):
+                self.screen.addstr(
+                    self.current_line,
+                    1,
+                    title,
+                )
+
+                self.screen.addstr(
+                    self.current_line,
+                    len(title) + 1,
+                    ch * current_bar,
+                    attrib,
+                )
+
+                self.screen.addstr(
+                    self.current_line,
+                    len(title) + current_bar,
+                    f"{value:.2f}",
+                    color,
+                )
+                # self.screen.getch()
+
+        except:
+            print(curses.error)
+            self.cleanup()
+            raise
+
+    def draw_vertical_bar(
+        self, title="title", value=0.0, color=curses.A_BOLD, attrib="normal"
+    ):
+        self.num_rows, self.num_cols = self.screen.getmaxyx()
+        self.current_row += 5
+        starting_line = int(self.num_rows / 2)
+        try:
+            bar_height = starting_line * 0.8
+            current_bar = int(bar_height * value)
+            ch = " "
+
+            for ii in range(current_bar, 0, -1):
+                if (
+                    self.current_line < self.num_rows
+                    and (starting_line) < self.num_cols
+                    and (starting_line - ii) > 2
+                    and starting_line - current_bar > 2
+                ):
+                    self.screen.addstr(starting_line - ii, self.current_row, ch, color)
+                    self.screen.addstr(
+                        starting_line - ii, self.current_row + 1, ch, color
+                    )
+
+                    # for ii in range(current_bar, 0, -1):
+                    self.screen.addstr(
+                        starting_line - current_bar - 1,
+                        self.current_row,
+                        f"{value:0.2f}",
+                    )
+                    self.screen.addstr(
+                        starting_line,
+                        self.current_row,
+                        f"{title}",
+                    )
+
+            # self.screen.getch()
+
+        except:
+            print(curses.error)
+            self.cleanup()
+            raise
+
+
+if __name__ == "__main__":
+    screen_debug = TerminalDashboard()
+    screen_debug.run()
+    screen_debug.cleanup()
---
+++ test_TD.py
@@ -0,0 +1,104 @@
+from terminal_dashboard import TerminalDashboard, PrintBlock, assigning_attrib
+
+
+if __name__ == "__main__":
+    deb = TerminalDashboard()
+
+    for i in range(10000000):
+        all_blocks = []
+        color = deb.NOMINAL
+
+        # Block 1
+        block = PrintBlock()
+        block.title = "POSITION:"
+        block.string_with_attrib = [
+            ["---", color],
+            [block.title, color],
+            [f" Position    : {i}", color],
+            [f" Orientation : {i}", color],
+            [f" Velocity    : {i}", color],
+        ]
+        all_blocks.append(block)
+
+        # Block 2
+        flag = False
+        block = PrintBlock()
+        block.title = "SAFETY:"
+        block.string_with_attrib = [
+            ["---", color],
+            [block.title, color],
+            [f" Position : {i}", color],
+            [f" Flag     : {flag}", assigning_attrib(flag)],
+            [f" Velocity : {i}", color],
+        ]
+        all_blocks.append(block)
+
+        # Block 3
+        block = PrintBlock()
+        block.title = "ANOTHER"
+        block.string_with_attrib = [
+            ["---", color],
+            [block.title, color],
+            [f" Position    : {i}", color],
+            [f" Orientation : {i}", color],
+            [f" Velocity    : {i}", color],
+        ]
+        all_blocks.append(block)
+
+        # Block 4
+        block = PrintBlock()
+        block.title = "YET ANOTHER"
+        block.string_with_attrib = [
+            ["---", color],
+            [block.title, color],
+            [f" Position    : {i}", color],
+            [f" Orientation : {i}", color],
+            [f" Velocity    : {i}", color],
+        ]
+        all_blocks.append(block)
+
+        # # Block 5
+        # block = PrintBlock()
+        # block.title = "FINAL"
+        # block.string_with_attrib = [
+        #     ["---", color],
+        #     [block.title, color],
+        #     [f" Position    : {i}", color],
+        #     [f" Orientation : {i}", color],
+        #     [f" Velocity    : {i}", color],
+        # ]
+        # all_blocks.append(block)
+
+        # Printing
+        deb.print_info(all_blocks, 1)
+
+        deb.all_refresh()
+        deb.all_erase()
+
+    #     # Drawing bars
+    #     bar = i / 100000
+    #     bar2 = (i + 15) / 100000
+
+    #     bar = 1 if bar > 1 else bar
+    #     bar2 = 1 if bar2 > 1 else bar2
+
+    #     deb.draw_bar(
+    #         title="Actual Speed: ", value=bar, color=deb.BAR_YELLOW, attrib="normal"
+    #     )
+
+    #     deb.draw_bar(
+    #         title="Actual Speed: ", value=bar2, color=deb.BAR_YELLOW, attrib="normal"
+    #     )
+    #     deb.draw_vertical_bar(
+    #         title="vz ", value=bar, color=deb.BAR_WHITE, attrib="normal"
+    #     )
+
+    #     deb.draw_vertical_bar(
+    #         title="vy ", value=0.5, color=deb.BAR_YELLOW, attrib="normal"
+    #     )
+
+    #     deb.draw_vertical_bar(
+    #         title="vx ", value=0.8, color=deb.BAR_WHITE, attrib="normal"
+    #     )
+
+    # deb.cleanup()
